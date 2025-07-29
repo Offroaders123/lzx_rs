b66bcbddadee186f2f97fe454051723a4e8391d9
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-07-28 22:28:43 -0700

    Initial ABI Types

---
+++ src/lzx.rs
@@ -0,0 +1,15 @@
+#[repr(C)]
+pub struct LzxState;
+
+unsafe extern "C" {
+    pub fn lzx_init(window: i32) -> *mut LzxState;
+    pub fn lzx_teardown(p_state: *mut LzxState) -> ();
+    pub fn lzx_reset(p_state: *mut LzxState) -> ();
+    pub fn lzx_decompress(
+        p_state: *mut LzxState,
+        inpos: *mut u8,
+        outpos: *mut u8,
+        inlen: i32,
+        outlen: i32,
+    ) -> i32;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod lzx;
+
 fn main() {
     println!("Hello, world!");
 }
