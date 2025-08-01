b8eb7538dc6a670d05bdae2d7e6d2ba468cedcb2
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-07-31 18:15:04 -0700

    Shorthand Enum Closure
    This is super cool! Not sure if I knew about this syntax. GPT recommended it.
    
    mindblur
    
    wow wow, again. Can't wait to make this ambient album. I might just have to include all of them in a single double album or something.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -10,9 +10,9 @@ enum DecompressError {
 
 #[test]
 fn decompress_xbox_360() -> Result<(), DecompressError> {
-    let world: Vec<u8> = read("tests/XBOX360_TU74.dat").map_err(|e| DecompressError::Io(e))?;
+    let world: Vec<u8> = read("tests/XBOX360_TU74.dat").map_err(DecompressError::Io)?;
     let mut unzipped: Vec<u8> = vec![];
-    x_decompress(&world, &mut unzipped).map_err(|e| DecompressError::XmemErr(e))?;
+    x_decompress(&world, &mut unzipped).map_err(DecompressError::XmemErr)?;
     println!("{:?}", unzipped);
     Ok(())
 }
