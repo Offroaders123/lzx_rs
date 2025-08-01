c70151db321218c1edf9c2790a23c32e3782d7f8
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-07-31 18:16:44 -0700

    From for Error
    This is super awesome. This totally changes how I understand Error handling and wrapping for Result types (I didn't understand it before, lol). This is very awesomely ergonomic. Nice to be able to use `?` with the flick of a finger! Just a keypress and a blink away.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -8,11 +8,23 @@ enum DecompressError {
     XmemErr(XmemErr),
 }
 
+impl From<io::Error> for DecompressError {
+    fn from(e: io::Error) -> Self {
+        DecompressError::Io(e)
+    }
+}
+
+impl From<XmemErr> for DecompressError {
+    fn from(e: XmemErr) -> Self {
+        DecompressError::XmemErr(e)
+    }
+}
+
 #[test]
 fn decompress_xbox_360() -> Result<(), DecompressError> {
-    let world: Vec<u8> = read("tests/XBOX360_TU74.dat").map_err(DecompressError::Io)?;
+    let world: Vec<u8> = read("tests/XBOX360_TU74.dat")?;
     let mut unzipped: Vec<u8> = vec![];
-    x_decompress(&world, &mut unzipped).map_err(DecompressError::XmemErr)?;
+    x_decompress(&world, &mut unzipped)?;
     println!("{:?}", unzipped);
     Ok(())
 }
