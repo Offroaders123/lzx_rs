a738d7f857ef2212558dbd62b1aa25552176af20
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-08-04 15:48:01 -0700

    360 File Reading
    Okay, now I am seeing how this works. Bit by bit, no pun intended. Well, now it's intended.

--- tests/xbox_360_dat.rs
+++ tests/xbox_360_dat.rs
@@ -2,14 +2,14 @@ use std::path::PathBuf;
 
 pub trait ConsoleParser {
     // fn discover_save_layout(&self, root_folder: &PathBuf) -> SaveLayout;
-    fn inflate_from_layout(&self, the_save: &SaveProject, in_file_path: &PathBuf) -> i32;
+    fn inflate_from_layout(&mut self, the_save: &SaveProject, in_file_path: &PathBuf) -> Status;
 
     // fn deflate_to_save(&self, save_project: &SaveProject, the_settings: &WriteSettings) -> i32;
     // fn supply_required_defaults(&self, save_project: &SaveProject) -> ();
 
     // protected:
 
-    fn inflate_listing(&self, save_project: &SaveProject) -> i32;
+    fn inflate_listing(&self, save_project: &SaveProject) -> Status;
     // fn deflate_listing(
     //     &self,
     //     game_data_path: &PathBuf,
@@ -26,7 +26,8 @@ pub struct WriteSettings;
 pub struct Buffer;
 
 pub struct Xbox360Dat {
-    pub console: Console,
+    m_console: Console,
+    m_file_path: Option<PathBuf>,
 }
 
 pub enum Console {
@@ -36,17 +37,43 @@ pub enum Console {
 impl Xbox360Dat {
     pub fn new() -> Self {
         Xbox360Dat {
-            console: Console::Xbox360,
+            m_console: Console::Xbox360,
+            m_file_path: None,
         }
     }
 }
 
 impl ConsoleParser for Xbox360Dat {
-    fn inflate_from_layout(&self, the_save: &SaveProject, in_file_path: &PathBuf) -> i32 {
-        todo!()
+    fn inflate_from_layout(&mut self, save_project: &SaveProject, the_file_path: &PathBuf) -> Status {
+        self.m_file_path = Some(the_file_path.clone());
+
+        let status: Status = self.inflate_listing(save_project);
+        match status {
+            Status::Compress => (),
+            _ => {
+                println!("failed to extract listing\n");
+                return status;
+            }
+        }
+
+        // readFileInfo(save_project);
+
+        return Status::Success;
     }
 
-    fn inflate_listing(&self, save_project: &SaveProject) -> i32 {
+    fn inflate_listing(&self, save_project: &SaveProject) -> Status {
         todo!()
     }
+}
+
+pub enum Status {
+    Success = 0,
+    Compress = -1,
+    Decompress = -2,
+    MallocFailed = -3,
+    InvalidSave = -4,
+    FileError = -5,
+    InvalidConsole = -6,
+    InvalidArgument = -7,
+    NotImplemented = -8,
 }
