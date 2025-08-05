82580e7d8e3fc634498caa16c2c9515f338036ac
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-08-04 17:13:28 -0700

    Variable Typings & Name Tidying

--- tests/xbox_360_dat.rs
+++ tests/xbox_360_dat.rs
@@ -4,23 +4,23 @@ use byteorder::{BigEndian, ReadBytesExt};
 use lzx_rs::x_decompress;
 
 pub trait ConsoleParser {
-    fn inflate_from_layout(&mut self, in_file_path: &PathBuf) -> Result<Vec<u8>, Status>;
+    fn inflate_from_layout(&mut self, file_path: &PathBuf) -> Result<Vec<u8>, Status>;
     fn inflate_listing(&self) -> Result<Vec<u8>, Status>;
 }
 
 pub struct Xbox360Dat {
-    m_file_path: Option<PathBuf>,
+    file_path: Option<PathBuf>,
 }
 
 impl ConsoleParser for Xbox360Dat {
-    fn inflate_from_layout(&mut self, the_file_path: &PathBuf) -> Result<Vec<u8>, Status> {
-        self.m_file_path = Some(the_file_path.clone());
+    fn inflate_from_layout(&mut self, file_path: &PathBuf) -> Result<Vec<u8>, Status> {
+        self.file_path = Some(file_path.clone());
 
         self.inflate_listing()
     }
 
     fn inflate_listing(&self) -> Result<Vec<u8>, Status> {
-        let file_path: &PathBuf = match &self.m_file_path {
+        let file_path: &PathBuf = match &self.file_path {
             Some(path) => path,
             None => Err(Status::FileError)?,
         };
@@ -33,7 +33,7 @@ impl ConsoleParser for Xbox360Dat {
         if file_data.len() < 12 {
             return Err(Status::FileError);
         }
-        let mut reader = Cursor::new(file_data);
+        let mut reader: Cursor<Vec<u8>> = Cursor::new(file_data);
 
         let src_size: u32 = match reader.read_u32::<BigEndian>() {
             Ok(val) => val.wrapping_sub(8),
